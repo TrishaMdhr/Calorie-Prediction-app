d11388509a00104a2022fd1255a30b99cd0894a1
Shrena Shakya <shakyashrena25@gmail.com>
2026-05-27 12:39:35 +0545

    Updated calorie map logic

--- calorie_map.py
+++ calorie_map.py
@@ -1,13 +1,24 @@
 import pandas as pd
 
-# load nutrition dataset
-df= pd.read_csv("datasets/Indian_Food_Nutrition_Processed.csv", on_bad_lines='skip')
 
-#functions to search food
+df1= pd.read_csv("datasets/Indian_Food_Nutrition_Processed.csv", on_bad_lines='skip')
+df2= pd.read_csv("datasets/daily_food_nutrition_dataset.csv", on_bad_lines='skip')
+
+df2 =  df2.rename(columns={
+    "Dish Name": "Food_Item",
+    "Fats (g)": "Fat (g)",
+    "Fibre (g)": "Fiber (g)",
+    "Free Sugar (g)": "Sugars (g)"
+})
+
+df = pd.concat([df1, df2], ignore_index=True)
+
+df= df.fillna(0)
+
 def get_food_details(food_name):
 
     #search matching food
-    food = df[df['Dish Name'].str.lower() == food_name.lower()]
+    food = df[df['Food_Item'].str.lower() == food_name.lower()]
 
     #check if food exists
     if food.empty:
@@ -18,7 +29,7 @@ def get_food_details(food_name):
 
     #return nutrition details
     return{
-        "dish_name": food['Dish Name'],
+        "dish_name": food['Food_Item'],
         "calories": food['Calories (kcal)'],
         "protein": food['Protein (g)'],
         "carbs": food['Carbohydrates (g)'],
@@ -27,7 +38,5 @@ def get_food_details(food_name):
         "sodium":food['Sodium (mg)']
     }
 
-# Test function
-#result = get_food_details("Chicken sandwich")
-#print(result)
+
 
